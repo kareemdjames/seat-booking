26d85be98a9669a8e0a4294ac170932d0c91dc69
Kareem James <kareemdjames@gmail.com>
2018-08-08 20:57:27 -0400

    added SeatList component

---
+++ src/components/SeatList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SeatList = ({ title, children }) => (
+  <div>
+    <h3>{title}</h3>
+    <ol className="list" >
+      {children}
+    </ol>
+  </div>
+)
+
+SeatList.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string.isReuqired
+}
+
+export default SeatList;
