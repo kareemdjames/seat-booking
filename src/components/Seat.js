de69a49af83036c01765e494e19b381f328d8993
Kareem James <kareemdjames@gmail.com>
2018-08-08 20:23:11 -0400

    created Seat.js

---
+++ src/components/Seat.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Seat = ({ number, price, status }) => (
+    <li className={'seatobj ' + status} key={number.toString()}>
+    	<input type="checkbox" disabled={status === 'booked' ? true : false} id={number.toString()} onClick={handleClick}/>
+			<label htmlFor={number.toString()}>
+				{number}
+			</label>
+		</li>
+)
+
+const handleClick = (event) => {
+	console.log("seat selected " + event.target.checked);
+}
+
+Seat.propTypes = {
+	number:PropTypes.number,
+	price:PropTypes.number,
+	status:PropTypes.string
+}
+
+export default Seat;
